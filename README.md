25b6ec4a87ea665282d3e85e4091f7fb246eb0d1
Ming-der Wang <mingderwang@gmail.com>
2022-05-04 23:13:29 +0800

    pre-release

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# cli-microbundle-ts
+# go-etherspot
+
+> To use this command for testing etherspot features
+
+## p2p payment channel
+
+> off-line payment
+
+## p2p payment with hub in the same network
+
+## p2p payment with hub and bridge cross networks
+
--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 
-require('yargs')
-  .scriptName("mintapp1")
-  .usage('$0 <cmd> [args]')
-  .command('hello [name]', 'welcome ter yargs!', (yargs) => {
-    yargs.positional('name', {
-      type: 'string',
-      default: 'Cambi',
-      describe: 'the name to say hello to'
-    })
-  }, function (argv) {
-    console.log('hello', argv.name, 'welcome to yargs!')
-  })
-  .help()
-  .argv
+require("yargs")
+  .scriptName("go-etherspot")
+  .usage("$0 <cmd> [args]")
+  .command(
+    "hello [name]",
+    "To run etherspot p2p payment tests",
+    (yargs) => {
+      yargs.positional("name", {
+        type: "string",
+        default: "Ming",
+        describe: "the name to say hello to",
+      });
+    },
+    function (argv) {
+      console.log("hello", argv.name, "welcome to yargs!");
+    }
+  )
+  .help().argv;
